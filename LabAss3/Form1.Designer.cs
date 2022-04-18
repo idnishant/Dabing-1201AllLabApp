8dc9e2219dcb76b47f98b8d0f39fd69793a8fc59
nishant <nishant.mca06@gmail.com>
2022-04-18 16:38:43 +0800

    Add project files.

---
+++ LabAss3/Form1.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace LabAss3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbCountry = new System.Windows.Forms.ComboBox();
+            this.radioMale = new System.Windows.Forms.RadioButton();
+            this.radioFemale = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.chkReading = new System.Windows.Forms.CheckBox();
+            this.chkPainting = new System.Windows.Forms.CheckBox();
+            this.radioMarried = new System.Windows.Forms.RadioButton();
+            this.radioUnmarried = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dtgCustomer = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.lblID = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgCustomer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Customer Name";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(172, 64);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(278, 26);
+            this.txtName.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(61, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Country";
+            // 
+            // cmbCountry
+            // 
+            this.cmbCountry.FormattingEnabled = true;
+            this.cmbCountry.Items.AddRange(new object[] {
+            "China",
+            "Japan",
+            "Korea",
+            "US"});
+            this.cmbCountry.Location = new System.Drawing.Point(172, 111);
+            this.cmbCountry.Name = "cmbCountry";
+            this.cmbCountry.Size = new System.Drawing.Size(199, 28);
+            this.cmbCountry.TabIndex = 3;
+            // 
+            // radioMale
+            // 
+            this.radioMale.AutoSize = true;
+            this.radioMale.Checked = true;
+            this.radioMale.Location = new System.Drawing.Point(49, 30);
+            this.radioMale.Name = "radioMale";
+            this.radioMale.Size = new System.Drawing.Size(68, 24);
+            this.radioMale.TabIndex = 4;
+            this.radioMale.TabStop = true;
+            this.radioMale.Text = "Male";
+            this.radioMale.UseVisualStyleBackColor = true;
+            // 
+            // radioFemale
+            // 
+            this.radioFemale.AutoSize = true;
+            this.radioFemale.Location = new System.Drawing.Point(202, 30);
+            this.radioFemale.Name = "radioFemale";
+            this.radioFemale.Size = new System.Drawing.Size(87, 24);
+            this.radioFemale.TabIndex = 5;
+            this.radioFemale.Text = "Female";
+            this.radioFemale.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioFemale);
+            this.groupBox1.Controls.Add(this.radioMale);
+            this.groupBox1.Location = new System.Drawing.Point(123, 175);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(371, 84);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Gender";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(61, 282);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Hobby";
+            // 
+            // chkReading
+            // 
+            this.chkReading.AutoSize = true;
+            this.chkReading.Checked = true;
+            this.chkReading.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkReading.Location = new System.Drawing.Point(172, 277);
+            this.chkReading.Name = "chkReading";
+            this.chkReading.Size = new System.Drawing.Size(95, 24);
+            this.chkReading.TabIndex = 8;
+            this.chkReading.Text = "Reading";
+            this.chkReading.UseVisualStyleBackColor = true;
+            // 
+            // chkPainting
+            // 
+            this.chkPainting.AutoSize = true;
+            this.chkPainting.Location = new System.Drawing.Point(325, 277);
+            this.chkPainting.Name = "chkPainting";
+            this.chkPainting.Size = new System.Drawing.Size(92, 24);
+            this.chkPainting.TabIndex = 9;
+            this.chkPainting.Text = "Painting";
+            this.chkPainting.UseVisualStyleBackColor = true;
+            // 
+            // radioMarried
+            // 
+            this.radioMarried.AutoSize = true;
+            this.radioMarried.Location = new System.Drawing.Point(29, 44);
+            this.radioMarried.Name = "radioMarried";
+            this.radioMarried.Size = new System.Drawing.Size(87, 24);
+            this.radioMarried.TabIndex = 10;
+            this.radioMarried.Text = "Married";
+            this.radioMarried.UseVisualStyleBackColor = true;
+            this.radioMarried.CheckedChanged += new System.EventHandler(this.radioMarried_CheckedChanged);
+            // 
+            // radioUnmarried
+            // 
+            this.radioUnmarried.AutoSize = true;
+            this.radioUnmarried.Checked = true;
+            this.radioUnmarried.Location = new System.Drawing.Point(173, 44);
+            this.radioUnmarried.Name = "radioUnmarried";
+            this.radioUnmarried.Size = new System.Drawing.Size(108, 24);
+            this.radioUnmarried.TabIndex = 11;
+            this.radioUnmarried.TabStop = true;
+            this.radioUnmarried.Text = "Unmarried";
+            this.radioUnmarried.UseVisualStyleBackColor = true;
+            this.radioUnmarried.CheckedChanged += new System.EventHandler(this.radioUnmarried_CheckedChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioUnmarried);
+            this.groupBox2.Controls.Add(this.radioMarried);
+            this.groupBox2.Location = new System.Drawing.Point(151, 329);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(342, 85);
+            this.groupBox2.TabIndex = 12;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Satus";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(151, 469);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 37);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Previews";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dtgCustomer
+            // 
+            this.dtgCustomer.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgCustomer.Location = new System.Drawing.Point(532, 79);
+            this.dtgCustomer.Name = "dtgCustomer";
+            this.dtgCustomer.RowHeadersWidth = 62;
+            this.dtgCustomer.RowTemplate.Height = 28;
+            this.dtgCustomer.Size = new System.Drawing.Size(1063, 373);
+            this.dtgCustomer.TabIndex = 14;
+            this.dtgCustomer.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtgCustomer_CellClick);
+            this.dtgCustomer.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtgCustomer_CellContentClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(318, 469);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(145, 37);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Add";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(151, 550);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(145, 35);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "Update";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Location = new System.Drawing.Point(94, 557);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(0, 20);
+            this.lblID.TabIndex = 17;
+            this.lblID.Visible = false;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(318, 550);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(145, 34);
+            this.button4.TabIndex = 18;
+            this.button4.Text = "Delete";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1649, 788);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.lblID);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dtgCustomer);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.chkPainting);
+            this.Controls.Add(this.chkReading);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.cmbCountry);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Customer Data Entry Form";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgCustomer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbCountry;
+        private System.Windows.Forms.RadioButton radioMale;
+        private System.Windows.Forms.RadioButton radioFemale;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox chkReading;
+        private System.Windows.Forms.CheckBox chkPainting;
+        private System.Windows.Forms.RadioButton radioMarried;
+        private System.Windows.Forms.RadioButton radioUnmarried;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dtgCustomer;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
